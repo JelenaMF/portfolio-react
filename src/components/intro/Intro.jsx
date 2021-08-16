50c372815faa74ac4b75bd90d6b0ba5e6f6c3428
Jelena Feliciano <jfel217@gmail.com>
2021-08-15 22:52:17 -0400

    added more content to into with images

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,22 @@ import'./intro.scss'
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            Intro
+            <div className="left">
+                <div className="imgContainer">
+                        <img src="assets/me_.jpg"  alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Jelena Feliciano</h1>
+                    <h3>Freelance <span>{ /**add changing titles */} </span> </h3>
+                
+                    <a href="#portfolio">
+                        <img src="assets/assets/downarrow_121316.png" alt="" />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
