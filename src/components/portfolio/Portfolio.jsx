029aab6ce258214e61073289e3590407f88db6ae
Jelena Feliciano <jfel217@gmail.com>
2021-08-16 11:43:48 -0400

    changed profile image

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,14 @@ import './portfolio.scss'
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            Portfolio
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
         </div>
     )
 }
