d6aa6a20ff3c7b937064579dee748a9c8b35858e
Jelena Feliciano <jfel217@gmail.com>
2021-08-16 10:57:30 -0400

    added animation to the h3 span using ityped

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,6 +7,14 @@ export default function Intro() {
     //npm doc for iTyped is written for class component using componentDidMount translate for a functional component using useEffect
     //this useEffect will work only one time because it's followed by an empty array
     useEffect(() =>{
+        //choose item
+        //initialize item
+        init(textRef.current, {
+            showCursor: true, //displays a cursor   
+            backDelay: 1500, //delays for a half a second before backspacing
+            backSpeed: 60, //type back speed in milliseconds doc found in npm ityped docs
+            strings: ["Developer", "Designer", "Content Creator"] //strings to replace text in h3 span tags
+        })
         console.log(textRef);
     }, [])
     return (
@@ -20,7 +28,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Jelena Feliciano</h1>
-                    <h3>Freelance <span ref={textRef}>Designer </span> </h3>
+                    <h3>Freelance <span ref={textRef}> </span> </h3>
                    
                 </div>
                 <a href="#portfolio">
