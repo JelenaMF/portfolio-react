41266a1d8740e840038024cca58ca0134f03f96e
Jelena Feliciano <jfel217@gmail.com>
2021-08-16 10:40:10 -0400

    installed ityped and imported it to Intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,14 @@
-import'./intro.scss'
+import'./intro.scss';
+import { init } from 'ityped';
+import {useEffect , useRef} from 'react'; 
 
 export default function Intro() {
+    const textRef = useRef();
+    //npm doc for iTyped is written for class component using componentDidMount translate for a functional component using useEffect
+    //this useEffect will work only one time because it's followed by an empty array
+    useEffect(() =>{
+        console.log(textRef);
+    }, [])
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +20,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Jelena Feliciano</h1>
-                    <h3>Freelance <span>{ /**add changing titles */} </span> </h3>
+                    <h3>Freelance <span ref={textRef}>Designer </span> </h3>
                    
                 </div>
                 <a href="#portfolio">
