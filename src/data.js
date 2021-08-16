60428c8076f10d317ca570e5b3705792e342890a
Jelena Feliciano <jfel217@gmail.com>
2021-08-16 13:46:51 -0400

    added portfolio data

---
+++ src/data.js
@@ -0,0 +1,95 @@
+import "../public/assets";
+export const featuredPortfolio = [
+     {
+         id: 1,
+         title: "Event Registration Form",
+         img: "../public/assets/eform.jpg"
+     },
+     {
+        id: 2,
+        title: "Employee Directory",
+        img: "../public/assets/empDir.jpg"
+    },
+    {
+        id: 3,
+        title: "Event Registration Form",
+        img: "../public/assets/eform.jpg"
+    },
+]
+
+export const webPortfolio = [
+    {
+        id: 1,
+        title: "Ruth Bader Ginsburg Quotes",
+        img: "../public/assets/rgb250.jpg"
+    },
+    {
+       id: 2,
+       title: "Event Registration Form",
+       img: "../public/assets/eform.jpg"
+   },
+   {
+       id: 3,
+       title: "Employee Directory",
+       img: "../public/assets/empDir.jpg"
+   },
+   {
+    id: 4,
+    title: "Link Four",
+    img: "../public/assets/link4.jpg"
+    }
+]
+
+export const mobilePortfolio = [
+    {
+        id: 1,
+        title: "What's the phrase? Game",
+        img: "../public/assets/game250.jpg"
+    },
+    {
+       id: 2,
+       title: "Event Registration Form",   /** update late  */
+       img: "../public/assets/eform.jpg"
+   },
+   {
+       id: 3,
+       title: "Event Registration Form",   /** update late  */
+       img: "../public/assets/eform.jpg"
+   },
+]
+
+export const designPortfolio = [
+    {
+        id: 1,
+        title: "Event Registration Form",
+        img: "../public/assets/eform.jpg"  /** update late  */
+    },
+    {
+       id: 2,
+       title: "Event Registration Form",  /** update late  */
+       img: "../public/assets/eform.jpg"
+   },
+   {
+       id: 3,
+       title: "Event Registration Form",  /** update late  */
+       img: "../public/assets/eform.jpg"
+   },
+]
+
+export const contentPortfolio = [    /** update late  */
+    {
+        id: 1,
+        title: "Event Registration Form",
+        img: "../public/assets/eform.jpg"
+    },
+    {
+       id: 2,
+       title: "Event Registration Form",
+       img: "../public/assets/eform.jpg"
+   },
+   {
+       id: 3,
+       title: "Event Registration Form",
+       img: "../public/assets/eform.jpg"
+   },
+]
